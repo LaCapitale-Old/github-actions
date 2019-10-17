4f651fe9e7a764d15fc653c7c27a63609dd2e0fc
Philippe-Olivier Berube <berube.po@gmail.com>
2019-10-17 14:18:22 -0400

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,11 +4,12 @@ set -e
 
 : ${GCLOUD_REGISTRY:=gcr.io}
 : ${IMAGE:=$(echo $GITHUB_REPOSITORY | sed -e 's/[\/]/-/g')}
+: ${GCP_PROJECT:=''}
 : ${TAG:=$GITHUB_SHA}
 : ${DEFAULT_BRANCH_TAG:=true}
 
 docker build -t $IMAGE:$TAG .
-docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$TAG
+docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$GCP_PROJECT/$IMAGE:$TAG
 if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
   BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
   if [ "$BRANCH" = "master" ]; then # TODO
