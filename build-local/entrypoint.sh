ae9292973d9a986dd6a7b7e4cd70dd861d9473d5
Philippe-Olivier Berube <berube.po@gmail.com>
2019-10-17 16:45:41 -0400

    Update entrypoint.sh

--- build-local/entrypoint.sh
+++ build-local/entrypoint.sh
@@ -6,12 +6,13 @@ set -e
 : ${IMAGE:='app'}
 : ${TAG:=$GITHUB_SHA}
 : ${DEFAULT_BRANCH_TAG:=true}
+: ${REPO:=$(echo $GITHUB_REPOSITORY | awk '{print tolower($0)}')}
 
 docker build -t $IMAGE:$TAG .
-docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$GITHUB_REPOSITORY/$IMAGE:$TAG
+docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$REPO/$IMAGE:$TAG
 if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
   BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
   if [ "$BRANCH" = "master" ]; then # TODO
-    docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$GITHUB_REPOSITORY/$IMAGE:$BRANCH
+    docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$REPO/$IMAGE:$BRANCH
   fi
 fi
