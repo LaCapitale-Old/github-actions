0b8ad5ecf8b838c1124d7b1f6ead8d692cb0b9ab
Philippe-Olivier Berube <berube.po@gmail.com>
2019-10-17 16:29:16 -0400

    Update entrypoint.sh

--- push-local/entrypoint.sh
+++ push-local/entrypoint.sh
@@ -2,19 +2,17 @@
 
 set -e
 
-: ${GCLOUD_REGISTRY:=gcr.io}
+: ${GCLOUD_REGISTRY:=docker.pkg.github.com}
 : ${IMAGE:=$(echo $GITHUB_REPOSITORY | sed -e 's/[\/]/-/g' | awk '{print tolower($0)}')}
 : ${GCP_PROJECT:=''}
 : ${TAG:=$GITHUB_SHA}
 : ${DEFAULT_BRANCH_TAG:=true}
 
-if [ -n "${GCLOUD_SERVICE_ACCOUNT_KEY}" ]; then
-  echo "Logging into gcr.io with GCLOUD_SERVICE_ACCOUNT_KEY..."
-  echo ${GCLOUD_SERVICE_ACCOUNT_KEY} | base64 --decode --ignore-garbage > /tmp/key.json
-  gcloud auth activate-service-account --quiet --key-file /tmp/key.json
-  gcloud auth configure-docker --quiet
+if [ -n "${GITHUB_ACCESS_TOKEN}" ]; then
+  echo "Logging into docker.pkg.github.com with GITHUB_ACCESS_TOKEN..."
+  docker login docker.pkg.github.com -u po.berube -p ${GITHUB_ACCESS_TOKEN}
 else
-  echo "GCLOUD_SERVICE_ACCOUNT_KEY was empty, not performing auth" 1>&2
+  echo "GITHUB_ACCESS_TOKEN was empty, not performing auth" 1>&2
 fi
 
 docker push $GCLOUD_REGISTRY/$GCP_PROJECT/$IMAGE
