f173577b051a972e2945d6904a50f019b2dfa54b
Philippe-Olivier Berube <berube.po@gmail.com>
2019-10-17 16:25:03 -0400

    Create entrypoint.sh

---
+++ build-local/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+: ${GCLOUD_REGISTRY:=gcr.io}
+: ${IMAGE:=$(echo $GITHUB_REPOSITORY | sed -e 's/[\/]/-/g' | awk '{print tolower($0)}')}
+: ${GCP_PROJECT:=''}
+: ${TAG:=$GITHUB_SHA}
+: ${DEFAULT_BRANCH_TAG:=true}
+
+docker build -t $IMAGE:$TAG .
+docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$GCP_PROJECT/$IMAGE:$TAG
+if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
+  BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
+  if [ "$BRANCH" = "master" ]; then # TODO
+    docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$BRANCH
+  fi
+fi
