e1ba9f3b547988ddccc039d1dacc4eed3867a540
Philippe-Olivier Berube <berube.po@gmail.com>
2019-10-18 11:19:59 -0400

    Update entrypoint.sh

--- push-local/entrypoint.sh
+++ push-local/entrypoint.sh
@@ -10,7 +10,7 @@ set -e
 
 if [ -n "${GITHUB_ACCESS_TOKEN}" ]; then
   echo "Logging into docker.pkg.github.com with GITHUB_ACCESS_TOKEN..."
-  docker login docker.pkg.github.com -u po.berube -p ${GITHUB_ACCESS_TOKEN}
+  docker login docker.pkg.github.com -u po.berube --password-stdin ${GITHUB_ACCESS_TOKEN}
 else
   echo "GITHUB_ACCESS_TOKEN was empty, not performing auth" 1>&2
 fi
