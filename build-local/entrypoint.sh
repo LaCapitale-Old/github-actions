eb6ceda48dedb866fb66454eacf42d488709265c
Philippe-Olivier Berube <berube.po@gmail.com>
2019-10-18 10:38:44 -0400

    Update entrypoint.sh

--- build-local/entrypoint.sh
+++ build-local/entrypoint.sh
@@ -8,6 +8,13 @@ set -e
 : ${DEFAULT_BRANCH_TAG:=true}
 : ${REPO:=$(echo $GITHUB_REPOSITORY | awk '{print tolower($0)}')}
 
+if [ -n "${GITHUB_ACCESS_TOKEN}" ]; then
+  echo "Logging into docker.pkg.github.com with GITHUB_ACCESS_TOKEN..."
+  docker login docker.pkg.github.com -u po.berube -p ${GITHUB_ACCESS_TOKEN}
+else
+  echo "GITHUB_ACCESS_TOKEN was empty, not performing auth" 1>&2
+fi
+
 docker pull $GCLOUD_REGISTRY/$REPO/$IMAGE:latest
 docker build -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$REPO/$IMAGE:$TAG
