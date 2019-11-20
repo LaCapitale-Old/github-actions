a5c543d4cef892c6dcaf484135642646b83e3590
Alexandre Boucher <alexandre.boucher@lacapitale.com>
2019-11-20 14:26:27 -0500

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,6 +8,15 @@ set -e
 : ${TAG:=$GITHUB_SHA}
 : ${DEFAULT_BRANCH_TAG:=true}
 
+if [ -n "${GCLOUD_SERVICE_ACCOUNT_KEY}" ]; then
+  echo "Logging into gcr.io with GCLOUD_SERVICE_ACCOUNT_KEY..."
+  echo ${GCLOUD_SERVICE_ACCOUNT_KEY} | base64 --decode --ignore-garbage > /tmp/key.json
+  gcloud auth activate-service-account --quiet --key-file /tmp/key.json
+  gcloud auth configure-docker --quiet
+else
+  echo "GCLOUD_SERVICE_ACCOUNT_KEY was empty, not performing auth" 1>&2
+fi
+
 docker build -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$GCP_PROJECT/$IMAGE:$TAG
 if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
