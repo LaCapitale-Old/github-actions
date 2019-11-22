f70cc6d4dc42a6466da45c6af6993eb4790d24e6
Philippe-Olivier Berube <berube.po@gmail.com>
2019-11-22 15:01:36 -0500

    Update entrypoint.sh

--- push-local/entrypoint.sh
+++ push-local/entrypoint.sh
@@ -12,7 +12,8 @@ if [ -n "${GITHUB_ACCESS_TOKEN}" && -n "${GITHUB_USERNAME}" ]; then
   echo "Logging into docker.pkg.github.com with GITHUB_ACCESS_TOKEN..."
   docker login docker.pkg.github.com -u ${GITHUB_USERNAME} -p ${GITHUB_ACCESS_TOKEN}
 else
-  echo "GITHUB_ACCESS_TOKEN was empty, not performing auth" 1>&2
+  echo "GITHUB_ACCESS_TOKEN or GITHUB_USERNAME was empty, not performing auth" 1>&2
+  exit 1
 fi
 
 docker push $GCLOUD_REGISTRY/$REPO/$IMAGE:$TAG
