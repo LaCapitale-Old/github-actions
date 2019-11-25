c0a97dacf03b050f8467ea972d2ee96e38a1db6e
Alexandre Boucher <alexandre.boucher@lacapitale.com>
2019-11-25 14:50:49 -0500

    Update entrypoint.sh

--- build-local/entrypoint.sh
+++ build-local/entrypoint.sh
@@ -11,7 +11,7 @@ set -e
 if [ "${GITHUB_ACCESS_TOKEN}" -a "${GITHUB_USERNAME}" ]; then
   echo "Logging into docker.pkg.github.com with GITHUB_ACCESS_TOKEN..."
   echo ${GCLOUD_SERVICE_ACCOUNT_KEY} | base64 --decode --ignore-garbage > /tmp/key.json
-  docker login docker.pkg.github.com -u gen01253 -p /tmp/key.json”
+  docker login docker.pkg.github.com -u ${GITHUB_USERNAME} -p /tmp/key.json”
   docker pull $GCLOUD_REGISTRY/$REPO/$IMAGE:latest || echo "image 'latest' not found, skip..."
 else
   echo "GITHUB_ACCESS_TOKEN was empty, not performing pull" 1>&2
