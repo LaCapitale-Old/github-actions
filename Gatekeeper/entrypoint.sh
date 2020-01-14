262f2db71ced3d267e41f89f6ea4e66f93d76851
Philippe-Olivier Berube <berube.po@gmail.com>
2020-01-14 15:34:04 -0500

    Update entrypoint.sh

--- Gatekeeper/entrypoint.sh
+++ Gatekeeper/entrypoint.sh
@@ -13,14 +13,16 @@ set -e
 : ${RUN_DIR:=''}
 : ${ARGS:=''}
 : ${CLUSTER:='DEV'}
-: ${GATEKEEPER_URL:='https://gatekeeper.infra1.lacapitale-gcp.com'}
+: ${GATEKEEPER_URL:='gatekeeper.infra1.lacapitale-gcp.com'}
+: ${GATEKEEPER_HTTP_PORT:='443'}
+: ${GATEKEEPER_RPC_PORT:='50051'}
 : ${GATEKEEPER_API_KEY:=''}
 
 if [ -n "${GCP_PROJECT_NAME}" ]; then
   echo "downloading client..."
   curl https://storage.googleapis.com/github-action-artefacts/cloudInfra-golang-gatekeeper-client/${CLIENT_VERSION}/cloudInfra-golang-gatekeeper-client_${CLIENT_VERSION}_Linux_x86_64.tar.gz | tar xvz
   echo "starting client..."
- ./cloudInfra-golang-gatekeeper-client -serverURL=${GATEKEEPER_URL}
+ ./cloudInfra-golang-gatekeeper-client -serverURL=${GATEKEEPER_URL} -serverPort=${GATEKEEPER_HTTP_PORT} -grpcPort=${GATEKEEPER_RPC_PORT}
 else
   echo "GCP_PROJECT_NAME was empty, we cannot call gatekeeper" 1>&2
 fi
